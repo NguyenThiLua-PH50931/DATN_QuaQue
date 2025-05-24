d665f3d17ec24a837c338095edf7b25d47e25ea7
NguyenThiLua-PH50931 <luantph50931@gmail.com>
2025-05-24 22:46:21 +0700

    update migrate

--- database/migrations/2025_05_24_153848_create_comments_table.php
+++ database/migrations/2025_05_24_153848_create_comments_table.php
@@ -11,10 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('carts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id'); 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->text('content');
+            $table->tinyInteger('rating')->unsigned()->nullable(); // 1–5 stars
+            $table->enum('status', ['visible', 'hidden'])->default('visible');
             $table->timestamps();
         });
     }
@@ -24,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('carts');
+        Schema::dropIfExists('comments');
     }
 };
