2f9f61ad224d7822961b92c49d6447767ffd86de
duyentt <duientrinh142@gmail.com>
2025-06-15 08:53:02 +0700

    update blog client

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -990,20 +990,8 @@
                                                     </div>
                                                 </li>
 
-                                                <li class="nav-item dropdown">
-                                                    <a class="nav-link dropdown-toggle" href="javascript:void(0)"
-                                                        data-bs-toggle="dropdown">Blog</a>
-                                                    <ul class="dropdown-menu">
-                                                        <li>
-                                                            <a class="dropdown-item" href="blog-detail.html">Blog Detail</a>
-                                                        </li>
-                                                        <li>
-                                                            <a class="dropdown-item" href="blog-grid.html">Blog Grid</a>
-                                                        </li>
-                                                        <li>
-                                                            <a class="dropdown-item" href="blog-list.html">Blog List</a>
-                                                        </li>
-                                                    </ul>
+                                                <li class="nav-item">
+                                                    <a href="{{ route('blog') }}">Tin tức</a>
                                                 </li>
 
                                                 <li class="nav-item dropdown new-nav-item">
