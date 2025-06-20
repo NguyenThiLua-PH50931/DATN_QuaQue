12f91254e19d95cd6c01475dce7cb8409fed7706
Hà <hoangha14102005@gmail.com>
2025-06-20 13:05:56 +0700

    update

--- resources/views/backend/header.blade.php
+++ resources/views/backend/header.blade.php
@@ -158,6 +158,3 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.bundle.min.js"></script>
 <script src="{{ asset('backend/assets/js/admin-search.js') }}"></script>
-
-
-{{-- <h1>Quân đẹp trai</h1> --}}
