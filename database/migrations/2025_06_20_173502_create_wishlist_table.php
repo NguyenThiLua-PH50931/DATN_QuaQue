dd09b7562ed86aedd41be1cbbc8e58fd02bbeb47
tuananh-teu <anhntph50475@gmail.com>
2025-06-21 02:41:38 +0700

    wishlist

--- database/migrations/2025_06_20_173502_create_wishlist_table.php
+++ database/migrations/2025_06_20_173502_create_wishlist_table.php
@@ -6,22 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('wishlist', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('wishlist');
     }
 };
