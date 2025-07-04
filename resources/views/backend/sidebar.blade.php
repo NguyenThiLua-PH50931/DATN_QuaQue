9dd77fd912ab10b76b149d038de2e5076dfcae0c
NguyenThiLua-PH50931 <luantph50931@gmail.com>
2025-07-05 00:32:41 +0700

    sửa

--- resources/views/backend/sidebar.blade.php
+++ resources/views/backend/sidebar.blade.php
@@ -17,9 +17,6 @@
         <div class="left-arrow" id="left-arrow"><i data-feather="arrow-left"></i></div>
         <div id="sidebar-menu" style="margin-top: -70px">
             <ul class="sidebar-links" id="simple-bar">
-                <li class="back-btn"></li> <br><br><br><br>
-                {{-- <li class="sidebar-list">
-                </li> --}}
                 <li class="sidebar-list">
                     <a class="sidebar-link" href="{{ url('/admin/reports') }}">
                         <i class="ri-list-check-2"></i>
