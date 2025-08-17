d47174e49033ece1699d0af3f68a621d112c2413
vumanhquan2005 <quanvvph50964@gmail.com>
2025-08-17 14:22:11 +0700

    ton kho khi thanh toan onl

--- database/seeders/ClearDiscountCodesSeeder.php
+++ database/seeders/ClearDiscountCodesSeeder.php
@@ -10,7 +10,7 @@ class ClearDiscountCodesSeeder extends Seeder
 public function run()
 {
     // Tắt ràng buộc foreign key tạm thời
-    DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+    // DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
     // DB::table('coupon_product')->truncate();
     // DB::table('discount_code_usages')->truncate();
@@ -18,11 +18,12 @@ public function run()
     // DB::table('discount_codes')->truncate();
     // DB::table('users')->truncate();
     // DB::table('addresses')->truncate();
-    DB::table('orders')->truncate();
-    DB::table('order_items')->truncate();
-    DB::table('order_status_logs')->truncate();
-    // DB::table('discount_codes')->truncate();
-    DB::table('cart_items')->truncate();
+    // DB::table('orders')->truncate();
+    // DB::table('order_items')->truncate();
+    // DB::table('order_status_logs')->truncate();
+    // // DB::table('discount_codes')->truncate();
+    // DB::table('cart_items')->truncate();
+     DB::table('reviews')->truncate();
     // Bật lại
     DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 }
